b91b82424bf951a215b0c717010e690d03131e15
syedhammadhussain <08hammad.hussain@gmail.com>
2020-04-14 19:58:22 +0500

    Creating Initial Cousera Repository

---
+++ stats.c
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Copyright (C) 2020 by Syed Hammad Hussain
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software.
+ *
+ *****************************************************************************/
+/**
+ * @file Stats.c
+ * @brief This program calculates 
+ *
+ * <Add Extended Description Here>
+ *
+ * @author Syed Hammad Hussain
+ * @date 14 April 2020
+ *
+ */
+
+
+#include <stdio.h>
+#include "stats.h"
+
+/* Size of the Data Set */
+#define SIZE (40)
+
+void main() {
+
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+
+  /* Other Variable Declarations Go Here */
+  /* Statistics and Printing Functions Go Here */
+
+}
+
+/* Add other Implementation File Code Here */
---
+++ stats.h
@@ -0,0 +1,46 @@
+/******************************************************************************
+ * Copyright (C) 2020 by Syed Hammad Hussain
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software.
+ *
+ *****************************************************************************/
+/**
+ * @file  Stats.h 
+ * @brief This is a header file for Stats.c and it contains decelarations of 
+ *	  functions used in Stats.c
+ *	  
+ * This file include decelarations of following functions:
+ *  print_statistics()
+ *  print_array()
+ *  find_median()
+ *  find_maximum()
+ *  find_minimum()
+ *  sort_array()
+ *
+ * @author Syed Hammad Hussain
+ * @date 14 April 2020
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
